0082ef043d3b6bf295ebf95739adb394af79579c
mrin2810 <mrinmayeerane2810@gmail.com>
2022-02-09 01:10:25 +0530

    initial app with apollo and css library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+// add
+// toggle
+// delete
+
+const GET_TODOS = gql`
+query fetchTodos {
+  Todos_Mrin_todos {
+    done
+    id
+    text
+  }
+}`
 
 function App() {
+  const {data, loading, error} = useQuery(GET_TODOS);
+  if(loading) return (<div>Loading...</div>)
+  if(error) return (<div>Error Fetching Data</div>)
+  // console.log(data.Todos_Mrin_todos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='vh-100 code flex flex-column items-center bg-purple white pa3 fl-1'>
+      <h1 className='f2-l'>
+        GraphQL Checklist
+        {" "}<span role='img' aria-label='Checkmark'>✅</span>
+      </h1>
+      {/* Todo Form */}
+      <form className='mb3'>
+        <input
+          className='pa2 f4 b--dashed'
+          type='text'
+          placeholder='Things ToDo'
+        />
+        <button
+          className='pa2 f4 bg-green' 
+          type='Submit'
+        >Create</button>
+      </form>
+      <div className='flex items-center justify-center flex-column'>
+        {data.Todos_Mrin_todos.map( (todo, id) => (
+          <p key={id}>
+            <span className='pointer pa1 list f3'>{todo.text}</span>
+            <button className='bg-transparent bn f4'><span className='red'>&times;</span></button>
+          </p>
+        ))}
+      </div>
+    </div>  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const graphQlAppLink = 'https://todo-react-mrin.hasura.app/v1/graphql';
+const client = new ApolloClient({
+  uri: graphQlAppLink,
+  headers: {
+    'x-hasura-admin-secret': 'AP3SCN6sFe1pdgWERNi3MvSMaKex7pEZpotw8W7clEHjagh2DFiIRXcpz367W2qo'
+  }
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
